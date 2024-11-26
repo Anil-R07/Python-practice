ecdbbe3f3214c94305e4bead4edc647565397afc
Anil-R07 <anilgowda.r18@gmail.com>
2024-11-26 20:11:23 +0530

    added variable.py

---
+++ variables.py
@@ -0,0 +1,28 @@
+#assignment 
+name = "Anil"      # string 
+age = 29           # Int 
+height = 5.11      # Float 
+is_employed = True # bool
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+print(is_employed)
+print(type(is_employed))
+
+#conversion of type 
+print(float(age))
+A= '90'
+print(int(A)+age)
+
+#reassignment 
+age = 30
+print(age)
+
+#
