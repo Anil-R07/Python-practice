456743e19103f39bfcad3136f68f79278ae1fe1c
Anil-R07 <anilgowda.r18@gmail.com>
2024-11-26 21:04:02 +0530

    learnt arithmetic operations

---
+++ arithmetic_operations.py
@@ -0,0 +1,34 @@
+# Addition (+)
+a = 1338
+b = 3323
+c = a+b
+print(c)
+
+# Subtraction (-)
+x = 3209
+y = 2342
+z = x-y
+print(z)
+
+# Multiplications (*)
+p = 192
+q = 323
+r = p*q
+print(r)
+
+# Division (/)
+e = 89
+f = 3
+g = 89/3
+print(g)
+
+# Floor devision (//)
+h= 89//3
+print(h)
+
+# Modulus (%)
+i = 89%3
+print(i)
+
+# Exponential (**)
+print(e**f)
