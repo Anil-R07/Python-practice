21d8dd35afe6bed62ef82e0d4f5ff7b2e15fce1e
Anil-R07 <anilgowda.r18@gmail.com>
2024-11-27 22:23:57 +0530

    learnt string manipulation

---
+++ Python_Basics/str_manipulation.py
@@ -0,0 +1,25 @@
+string = "Welcome All to This Page "
+# Upper case 
+print(string.upper())
+
+# Lower case 
+print(string.lower())
+
+# strip - Remove spaces at last
+print(string.strip())
+
+# replace 
+name = "John" 
+print(name.replace("John", "Joseph"))
+
+# Accessing string characters 
+print(string[5])
+print(string[-5])
+print(string[5:16]) # slicing 
+print(string[:8]) # from start to 8th index
+print(string[3:]) # from 3rd index to end 
+
+# Escape sequesnce 
+Text = "Hello my name is ABC and \ni am working as a Automation Engineer"
+print(Text)
+ 
