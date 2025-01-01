7bbfbece7114be4491367aabfed4cc0e04d140db
Anil-R07 <anilgowda.r18@gmail.com>
2025-01-01 20:57:21 +0530

    Added operators.py file

---
+++ Python_Basics/operators.py
@@ -0,0 +1,35 @@
+# Assignment  operators
+a = 10
+a+= 20    # a = a+20
+print(a)
+
+b = 10
+b-= 5
+print(b)  # b = b-5
+
+c = 10
+c*= 20
+print(c)  # c = c*20
+
+d = 10
+d/= 2
+print(abs(d))  # d = d/5
+
+# Comparison operators
+x = 50 
+y = 100
+
+print(x == y) # False 
+print(x != y) # True 
+print(x > y)  # False
+print(x < y)  # True
+print(x >= 10)# True
+print(y <= 120)# True
+
+# Logical Operators
+p = 10
+q = 20
+r = 25
+
+print(p > 5 and r > 10)  # True
+print(p > 10 or r > 10)  
