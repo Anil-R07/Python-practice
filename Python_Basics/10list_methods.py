e2aac98f2a85eeb6ee57be449127fa9243d492f9
Anil-R07 <anilgowda.r18@gmail.com>
2025-02-28 22:23:52 +0530

    added advance topic for list

--- Python_Basics/10list_methods.py
+++ Python_Basics/10list_methods.py
@@ -21,7 +21,17 @@ print(l)
 index = l.index(400)  #returns the index of the element 400
 print(index)
 
+sum = sum(l)  #adding the all the elements in a list
+print(sum)
+
 l.clear()  #remove all the data from list
 print(l)
 
 
+#Advanced topic 
+numbers = [1, 2,"Anil", "Teju", 3, 4]
+# sum(numbers) ---> This will give type error since it containd string 
+new_numbers = [x for x in numbers if isinstance(x, (int, float))] #o/p --> [1, 2, 3, 4]
+print(new_numbers)
+total = sum(new_numbers)
+print(total)
