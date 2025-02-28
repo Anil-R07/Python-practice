55e3ce2f061c806d0c4c68220916478122c1b5ac
Anil-R07 <anilgowda.r18@gmail.com>
2025-02-28 22:40:49 +0530

    updated matrix

---
+++ Python_Basics/11matrix.py
@@ -0,0 +1,9 @@
+#Matrix / nested list
+matrix = [[1,2], [2,3], [3,4], [4,5]]
+print(matrix)
+
+print(matrix[1]) #Get the first index list O/P: [2,3]
+
+print(matrix[1][1])  #Get the first index of first list O/P: 3
+
+#https://www.geeksforgeeks.org/python-matrix/
