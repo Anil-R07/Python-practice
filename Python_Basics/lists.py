eed413682a7ecc68767a7a119db3eb3d27bd470c
Anil-R07 <anilgowda.r18@gmail.com>
2025-02-28 21:37:21 +0530

    Added Lists and slice the list

---
+++ Python_Basics/lists.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "banana", "cherry", "orange"]  #declaring th list 
+print(fruits)  #print the list 
+
+fruits.append("water melon") #adding a new fruit to the list
+print(fruits)
+
+fruits.pop() #removing the last element in the list 
+print(fruits)
+
+fruits.pop(1) #removing the specific element from a list using index
+print(fruits)
+
+fruits.insert(1,"mango") #adding the element to the specific index
+print(fruits)
+
+fruits[2] = "pineapple"
+print(fruits)
---
+++ Python_Basics/sliceList.py
@@ -0,0 +1,11 @@
+#Slice 
+l1 = [1,2,3,4,5,6,7,8,9,10]
+
+l2 = l1[0:3] #slice from 0th index to 2nd index, it include till 3rd index
+print(l2)
+
+l3 = l1[:5]  #slice from start and upto 5th index 
+print(l3)
+
+l4 = l1[3:]  #slice from  3rd index to end
+print(l4)
