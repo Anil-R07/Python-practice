ad48725cf1e146498cc7d60a978624b2f509fbef
Anil-R07 <anilgowda.r18@gmail.com>
2025-03-12 21:15:08 +0530

    Added functions

---
+++ Python_Basics/18functions.py
@@ -0,0 +1,13 @@
+def ipl():
+    print("Tala Ipl 2025")
+
+ipl()
+
+def team(name):
+    if name == "RCB":
+        print("King Kohli")
+    elif name == "CSK":
+        print("MS Dhoni")
+    else:
+        print("No player found")
+team(input("Please Enter the team name: "))
