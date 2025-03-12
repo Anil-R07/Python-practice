f44510e66904e20fd72af9ba206e32103662f6e7
Anil-R07 <anilgowda.r18@gmail.com>
2025-03-12 21:30:19 +0530

    Updated functions examples

--- Python_Basics/18functions.py
+++ Python_Basics/18functions.py
@@ -11,3 +11,23 @@ def team(name):
     else:
         print("No player found")
 team(input("Please Enter the team name: "))
+
+#add 2 numbers 
+def add(a, b):
+    print(f"sum of the provided number is: {a+b}")
+num1 = int(input("please provide the first number: "))
+num2 = int(input("please provide the second number: "))
+add(num1, num2)
+
+#default parameter 
+def car(favcar = "Defender110"):
+    print(f"{favcar} is my favorite car")
+car("ferari")
+
+#local and global variable 
+def variable():
+    name = "Anil"
+    print(name)
+    print(name1)
+name1 = "Akash"
+variable()
