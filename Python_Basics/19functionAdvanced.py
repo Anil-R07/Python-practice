8acf2ff885d144c16b2765db6e20c54e634ba30b
Anil-R07 <anilgowda.r18@gmail.com>
2025-03-15 19:46:46 +0530

    updated advanced functions

--- Python_Basics/19functionAdvanced.py
+++ Python_Basics/19functionAdvanced.py
@@ -16,3 +16,27 @@ def team_info(**info):
     for key, value in info.items():
         print(f"{key}:{value}")
 team_info(name="RCB", place="Bengaluru", captain="Kohli", viceCaptain="Deviliers")
+
+#lambda functions
+triple = lambda x : x * 3
+print(triple(7))
+
+double = lambda x,y : x+y
+print(double(4,5))
+
+#recursion
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
+
+#nested functions
+def outer(name):
+    print(f"Hello {name} this is outer function")
+    def inner():
+        print(f"Hello {name} this is inner function")
+    inner()
+outer("Anil")
+    
