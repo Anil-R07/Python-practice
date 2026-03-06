933fc36860eccdf0a1dff7213977ffb7d96bb55b
Anil  R <anil.r@talview.com>
2026-03-06 21:51:44 +0530

    added for loop program

--- Python_Practice/if_conditions.py
+++ Python_Practice/if_conditions.py
@@ -150,39 +150,3 @@ else:
         elif num < 0:
             print(f"{num} is Negative Odd")
 
-for i in range(3):
-    for j in range(2):
-        print(i, j)
-
-
-for i in range(1,6):
-    if i == 3:
-        continue
-    print(i)
-
-
-for i in range (1, 21):
-    print(i)
-
-for i in range (20,0,-1):
-    print(i)
-
-#Print all even numbers between 1 and 50
-
-for i in range (1, 51):
-    if i%2 == 0:
-        print(i)
-
-#Print all odd numbers between 1 and 50
-for i in range (1, 51):
-    if i%2 !=0:
-        print(i)
-
-#Print the square of numbers from 1 to 10
-for i in range (11):
-    square = i*i
-    print(square)
-
-#Find the sum of numbers from 1 to N
-
-
