a2c09c65d68cfb3634a9ef791f206a1c953fa393
Anil-R07 <anilgowda.r18@gmail.com>
2026-04-05 19:55:17 +0530

    Added functions topic

---
+++ Python_Practice/func.py
@@ -0,0 +1,34 @@
+'''
+def greet():
+    print("Hello Anil")
+
+greet()
+greet()
+
+
+def greet(name):
+    print("Hello", name)
+
+greet("Anil")
+greet("Rahul")
+
+
+
+def add(a, b):
+    return a + b
+
+result = add(3, 4)
+print(result)
+
+def add(a,b):
+    return (a+b)
+
+result = add(3,4)
+print(result)
+'''
+
+def test():
+    return 5
+    print("Hello")
+
+print(test())
